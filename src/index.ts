4303c706cb81cc3d52e4b28413821f26c762fed5
vamshi126 <vamshi1264748@gmail.com>
2022-12-21 14:56:10 +0530

    new

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import DiscordJS, {  EmbedBuilder,GatewayIntentBits, TextChannel,    } from 'discord.js'
+import dotenv from 'dotenv'
+
+export const client =new DiscordJS.Client({
+    intents:[
+      
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+
+    ]
+})
+
+dotenv.config()
+client.login(process.env.TOKEN)
+
+
+
+
+
+client.on('ready',()=>{
+    console.log("Testing..")
+    client.user?.setStatus('idle')
+    client.user?.setPresence(
+    {activities:
+    [
+        {name:'Bolt!'}
+    ]
+    }) 
+  
+})
+
+
+
+
+
